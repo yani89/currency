494701f3ffe0b050a9558d01f1abcffa801f2733
Yaniar.Prayogo <yaniardev@gmail.com>
2018-06-28 15:45:23 +0700

    v2

---
+++ App.js
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  Image,
+  View
+} from 'react-native';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers, compose} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import reducers from './app/reducers'
+import AppContainer from './app/containers/AppContainer'
+
+import {persistStore, autoRehydrate} from 'redux-persist'
+import {AsyncStorage} from 'react-native'
+
+//define logger middleware: set logger for only development mode
+const loggerMiddleware = createLogger({ predicate: (getState, action) => __DEV__ });
+
+function configureStore(initialState) {
+  //more boilerplate. Enhancer composes different middleware
+  const enhancer = compose(
+    autoRehydrate(),
+    applyMiddleware(
+      thunkMiddleware,
+      loggerMiddleware,
+    ),
+  );
+  return createStore(reducers, initialState, enhancer);
+}
+
+const store = configureStore({});
+
+persistStore(store, {storage: AsyncStorage})
+
+const App = () => (
+  <Provider store={store}>
+    <AppContainer/>
+  </Provider>
+)
